2b719a0c799ea9abdf50201419160f2025df2725
Ronney Santiago <ronneysantiago@hotmail.com>
2018-06-26 12:36:58 -0300

    Adicionado retorno Json

---
+++ SD/Controllers/WebAPIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SD.Models;
+
+namespace SD.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        // GET: API
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getDDD(string cidade)
+        {
+            return Json(operador.getDDD(cidade), JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult getCidades(string ddd)
+        {
+            return Json(operador.getCidades(ddd), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
