6aa8521776c63f799fd98f74a72a9a82f2a7e305
Ronney Santiago <ronneysantiago@hotmail.com>
2018-06-30 14:02:00 -0300

    Iniciado Viewer

---
+++ SD/Controllers/ViewerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SD.Controllers
+{
+    public class ViewerController : Controller
+    {
+        // GET: Viewer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
